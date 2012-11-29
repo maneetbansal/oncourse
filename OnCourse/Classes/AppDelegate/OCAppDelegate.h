6d26a641b67b57f0a82198c928b0a29dcb118a50
PhatLe <phat.le@eastagile.com>
2012-11-29 22:59:11 +0700

    Reorganize the structure of files and folder

---
+++ OnCourse/Classes/AppDelegate/OCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  OCAppDelegate.h
+//  OnCourse
+//
+//  Created by East Agile on 11/29/12.
+//  Copyright (c) 2012 phatle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OCViewController;
+
+@interface OCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) OCViewController *viewController;
+
+@end
---
+++ OnCourse/Classes/Controllers/OCViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCViewController.h
+//  OnCourse
+//
+//  Created by East Agile on 11/29/12.
+//  Copyright (c) 2012 phatle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCViewController : UIViewController
+
+@end
